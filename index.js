19a1c2e9aba1df80f039173e0a9dbf4be0fcdbea
maxim <maxim218maxim218@yandex.ru>
2018-04-03 16:55:13 +0300

    Kill reading from files

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let app = express();
 app.use(express.static(__dirname + "/static"));
 
 app.get('/*', function(req, res) {
+    res.header("Cache-Control", "max-age=86400");
     res.sendfile("static/index.html");
 });
 
